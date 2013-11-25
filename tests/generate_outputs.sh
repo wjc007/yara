a91793208ac83a59ffd40dbe3f9134d0d9854883
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2013-11-25 16:24:46 +0100

    [TEST] added app test scripts

--- tests/generate_outputs.sh
+++ tests/generate_outputs.sh
@@ -2,7 +2,7 @@
 #
 # Masai reference output generation. Version v0.6.1 was used.
 
-PATH=~/Code/seqan-builds/Release-Gcc/bin/
+PATH=~/Code/seqan-builds/Release-Gcc/bin
 INDEXER=$PATH/cuda_indexer
 MAPPER=$PATH/cuda_mapper
 
@@ -10,24 +10,24 @@ MAPPER=$PATH/cuda_mapper
 # Run Indexer
 # ============================================================
 
-# Run with different organisms.
-for i in celegans; do #hg18
-    ${INDEXER} datasets/$i/genome.fasta &> logs/indexer.$i.stdout
+# Run with different organismnisms.
+#for organism in celegans hg18; do
+for organism in celegans; do
+    ${INDEXER} datasets/$organism/genome.fasta -xp datasets/$organism/genome.fm &> logs/indexer.$organism.stdout
 done
 
 # ============================================================
 # Run Single-End Mapper
 # ============================================================
 
+# Run on CPU with 1 thread, on CPU with 8 threads, on GPU.
 MAPPER_ARGS=("--no-cuda --threads 1" "--no-cuda --threads 8" "")
+LOG_SUFFIX=("t1" "t8" "cuda")
 
-# Run with different organisms.
-for i in celegans hg18; do
-    # Run with different seed length.
-#    for sl in 20 33; do
-        # Run on CPU with 1 thread,  on CPU with 8 threads, on GPU.        
-        for args in "${MAPPER_ARGS[@]}"; do
-            ${MAPPER} datasets/$i/genome.fasta datasets/$i/reads.fastq ${args} &> logs/mapper.$i.stdout
-        done
-#    done
+# Run with different organismnisms.
+for organism in celegans hg18; do
+    # Run with different arguments.
+    for ((a=0; a<${#MAPPER_ARGS[@]}; a++)); do
+        ${MAPPER} datasets/$organism/genome.fasta -xp datasets/$organism/genome.fm datasets/$organism/reads.fastq ${MAPPER_ARGS[$a]} &> logs/mapper.$organism.${LOG_SUFFIX[$a]}.stdout
+    done
 done
