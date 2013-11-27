75e3d01beaa28b76ac494e744b66eb73e5150990
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2013-11-27 16:04:44 +0100

    [INTERNAL] moved Mapper and Options into mapper.h

--- mapper.cpp
+++ mapper.cpp
@@ -70,10 +70,6 @@
 using namespace seqan;
 
 // ============================================================================
-// Classes
-// ============================================================================
-
-// ============================================================================
 // Functions
 // ============================================================================
 
@@ -173,247 +169,24 @@ parseCommandLine(Options & options, ArgumentParser & parser, int argc, char cons
 }
 
 // ----------------------------------------------------------------------------
-// Function configureThreads()
-// ----------------------------------------------------------------------------
-
-template <typename TExecSpace>
-void configureThreads(Mapper<TExecSpace> & /* mapper */, Options const & options)
-{
-    // Set the number of threads that OpenMP can spawn.
-    omp_set_num_threads(options.threadsCount);
-    std::cout << "Threads count:\t\t\t" << omp_get_max_threads() << std::endl;
-}
-
-// ----------------------------------------------------------------------------
-// Function loadGenome()
-// ----------------------------------------------------------------------------
-
-template <typename TExecSpace>
-void loadGenome(Mapper<TExecSpace> & mapper, Options const & options)
-{
-    // Load genome.
-    open(mapper.genomeLoader, options.genomeFile);
-
-    std::cout << "Loading genome:\t\t\t";
-    start(mapper.timer);
-    load(mapper.genomeLoader);
-    stop(mapper.timer);
-    std::cout << mapper.timer << std::endl;
-}
-
-// ----------------------------------------------------------------------------
-// Function loadGenomeIndex()
-// ----------------------------------------------------------------------------
-
-template <typename TExecSpace>
-void loadGenomeIndex(Mapper<TExecSpace> & mapper, Options const & options)
-{
-    std::cout << "Loading genome index:\t\t";
-    start(mapper.timer);
-    load(mapper.genomeIndex, options.genomeIndexFile);
-    stop(mapper.timer);
-    std::cout << mapper.timer << std::endl;
-}
-
-// ----------------------------------------------------------------------------
-// Function loadReads()
-// ----------------------------------------------------------------------------
-
-template <typename TExecSpace>
-void loadReads(Mapper<TExecSpace> & mapper, Options const & options)
-{
-    std::cout << "Loading reads:\t\t\t";
-    start(mapper.timer);
-    load(mapper.readsLoader, options.mappingBlock);
-    stop(mapper.timer);
-    std::cout << mapper.timer << std::endl;
-
-    std::cout << "Reads count:\t\t\t" << mapper.reads.readsCount << std::endl;
-}
-
-// ----------------------------------------------------------------------------
-// Function mapReads()
-// ----------------------------------------------------------------------------
-
-template <typename TExecSpace>
-void mapReads(Mapper<TExecSpace> & mapper, Options const & options)
-{
-    _mapReads(mapper.genomeIndex.index, getSeqs(mapper.reads), options.seedLength, options.errorsPerSeed, TExecSpace());
-}
-
-// ----------------------------------------------------------------------------
-// Function runMapper()
-// ----------------------------------------------------------------------------
-
-template <typename TExecSpace>
-void runMapper(Mapper<TExecSpace> & mapper, Options const & options)
-{
-#ifdef _OPENMP
-    configureThreads(mapper, options);
-#endif
-
-#ifdef ENABLE_GENOME_LOADING
-    loadGenome(mapper, options);
-#endif
-
-    loadGenomeIndex(mapper, options);
-
-    // Open reads file.
-    open(mapper.readsLoader, options.readsFile);
-
-    // Reserve space for reads.
-    reserve(mapper.reads, options.mappingBlock);
-
-    // Process reads in blocks.
-    while (!atEnd(mapper.readsLoader))
-    {
-        // Load one block of reads.
-        loadReads(mapper, options);
-
-        // Map this block of reads.
-        mapReads(mapper, options);
-
-        // Clear mapped reads.
-        clear(mapper.reads);
-    }
-
-    // Close reads file.
-    close(mapper.readsLoader);
-}
-
-// ----------------------------------------------------------------------------
-// Function runMapper()
-// ----------------------------------------------------------------------------
-
-#if false
-template <typename TExecSpace>
-void runMapper(Mapper<TExecSpace> & mapper, Options const & options)
-{
-    typedef Timer<double>                               TTimer;
-    typedef Logger<std::ostream>                        TLogger;
-    typedef Mapper<TExecSpace>                             TMapper;
-    typedef Reads<void, typename TMapper::TReadsConfig>    TReads;
-
-    TTimer timer;
-    TLogger cout(std::cout);
-    TLogger cerr(std::cerr);
-
-#ifdef _OPENMP
-    cout << "Threads count:\t\t\t" << options.threadsCount << std::endl;
-#endif
-
-#ifdef _OPENMP
-    // Disable nested parallelism.
-    omp_set_nested(false);
-#endif
-
-#ifdef ENABLE_GENOME_LOADING
-    loadGenome(mapper, options);
-#endif
-
-    loadGenomeIndex(mapper, options);
-
-    // Open reads file.
-    open(mapper.readsLoader, options.readsFile);
-
-    // Process reads in parallel.
-    SEQAN_OMP_PRAGMA(parallel firstprivate(timer) num_threads(3))
-    {
-        // Reserve space for reads.
-        TReads reads;
-        reserve(reads, options.mappingBlock);
-
-        // Process reads.
-        while (true)
-        {
-            // Load a block of reads.
-            SEQAN_OMP_PRAGMA(critical(_mapper_readsLoader_load))
-            {
-                // No more reads.
-                if (!atEnd(mapper.readsLoader))
-                {
-                    start(timer);
-                    setReads(mapper.readsLoader, reads);
-                    load(mapper.readsLoader, options.mappingBlock);
-                    stop(timer);
-
-                    cout << "Loading reads:\t\t\t" << timer << "\t\t[" << omp_get_thread_num() << "]" << std::endl;// <<
-//                            "Reads count:\t\t\t" << reads.readsCount << "\t\t\t[" << omp_get_thread_num() << "]" << std::endl;
-                }
-            }
-
-            // No more reads.
-            if (!reads.readsCount) break;
-
-            // Map reads.
-            SEQAN_OMP_PRAGMA(critical(_mapper_mapReads))
-            {
-                #ifdef _OPENMP
-                // Enable nested parallelism.
-                omp_set_nested(true);
-                #endif
-
-                #ifdef _OPENMP
-                omp_set_num_threads(options.threadsCount);
-                #endif
-
-                start(timer);
-                mapReads(mapper, options, getSeqs(reads));
-                stop(timer);
-
-                cout << "Mapping reads:\t\t\t" << timer << "\t\t[" << omp_get_thread_num() << "]" << std::endl;
-
-                #ifdef _OPENMP
-                omp_set_num_threads(1);
-                #endif
-
-                #ifdef _OPENMP
-                // Disable nested parallelism.
-                omp_set_nested(false);
-                #endif
-            }
-
-            // Writer results.
-            SEQAN_OMP_PRAGMA(critical(_mapper_samWriter_write))
-            {
-                start(timer);
-                sleep(reads.readsCount / 1000000.0);
-                stop(timer);
-                
-                cout << "Writing results:\t\t" << timer << "\t\t[" << omp_get_thread_num() << "]" << std::endl;
-            }
-
-            // Clear mapped reads.
-            clear(reads);
-        }
-    }
-
-    // Close reads file.
-    close(mapper.readsLoader);
-}
-#endif
-
-// ----------------------------------------------------------------------------
 // Function configureMapper()
 // ----------------------------------------------------------------------------
 
-template <typename TOptions>
-int configureMapper(TOptions & options)
+int configureMapper(Options const & options)
 {
 #ifndef CUDA_DISABLED
     if (options.noCuda)
     {
+#endif
         Mapper<ExecHost> mapper;
         runMapper(mapper, options);
+#ifndef CUDA_DISABLED
     }
     else
     {
         Mapper<ExecDevice> mapper;
         runMapper(mapper, options);
     }
-#else
-    Mapper<ExecHost> mapper;
-    runMapper(mapper, options);
 #endif
 
     return 0;
