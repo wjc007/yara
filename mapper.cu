4da6ed9f147c163c44268e5d151a6fafd672c251
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2014-03-16 23:45:26 +0100

    Split verifier and selector files

--- mapper.cu
+++ mapper.cu
@@ -74,6 +74,7 @@
 #include "mapper_filter.h"
 #include "mapper_extender.h"
 #include "mapper_verifier.h"
+#include "mapper_selector.h"
 #include "mapper_aligner.h"
 #include "mapper_writer.h"
 #include "mapper.h"
